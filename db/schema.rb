13e2f6c717f4a848630bf3400423def73d23b24c
Pierre Brossard <pierre.brossard496186@gmail.com>
2024-05-27 15:18:35 +0200

    add booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_131107) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_131746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_131107) do
     t.index ["user_id"], name: "index_aircrafts_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.string "pending"
+    t.date "start_date"
+    t.date "end_date"
+    t.decimal "total_price"
+    t.bigint "aircraft_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["aircraft_id"], name: "index_bookings_on_aircraft_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +54,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_131107) do
   end
 
   add_foreign_key "aircrafts", "users"
+  add_foreign_key "bookings", "aircrafts"
+  add_foreign_key "bookings", "users"
 end
