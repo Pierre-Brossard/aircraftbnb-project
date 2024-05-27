9665cc45ea2fb25b141830b5995fb72e2f47f939
Louis Tamagny <louis.tamagny@gmail.com>
2024-05-27 16:07:03 +0200

    aircraft model validations

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -5,12 +5,17 @@ class AircraftsController < ApplicationController
 
   def create
     @aircraft = Aircraft.new(aircraft_params)
-    @aircraft
+    @aircraft.user = current_user
+    if @aircraft.save
+      redirect_to aircrafts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def aircraft_params
-    params.require(:aircraft).permit(:name, :category, :description)
+    params.require(:aircraft).permit(:name, :category, :description, :capacity, :range, :state, :day_price)
   end
 end
