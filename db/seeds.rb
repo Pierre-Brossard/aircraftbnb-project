ad7fffa66841a2850263404970c60b63e8d8fcf7
Louis Tamagny <louis.tamagny@gmail.com>
2024-05-28 11:42:29 +0200

    seeds not working / waiting for bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,49 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+AIRCRAFT_NAMES = [
+      'Airco DH.4A',
+   ' Airco DH.9C',
+   ' Airco DH.16',
+   ' BAT F.K.26',
+  '  Blériot-SPAD S.27',
+   ' Curtiss Eagle',
+    'Farman F.50P',
+    'Farman F.60 Goliath',
+    'Grahame-White Charabanc',
+    'Junkers F.13',
+    'Nieuport-Delage NiD 30',
+    'Potez SEA VII',
+    'Sopwith Wallaby',
+    'Westland Limousine'
+]
+
+User.destroy_all
+Aircraft.destroy_all
+
+
+user = User.new
+user.name = 'Maria'
+user.email = 'test@example.com'
+user.encrypted_password = '#$taawktljasktlw4aaglj'
+user.save!
+
+user_bis = User.new
+user_bis.name = 'Bobby'
+user_bis.email = 'test2@example.com'
+user_bis.encrypted_password = '#$taawktlPPPPtlw4aaglj'
+user_bis.save!
+
+8.times do
+  Aircraft.create!(
+    name: AIRCRAFT_NAMES.sample,
+    category: Aircraft::CATEGORIES.sample,
+    description: 'An empty description',
+    capacity: (50..3500).sample,
+    range: (100..150_000).sample,
+    state: Aircraft::STATES.sample,
+    day_price: (1000..100_000).sample
+  )
+en
