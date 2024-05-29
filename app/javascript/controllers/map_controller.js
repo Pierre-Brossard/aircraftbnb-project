ef123b0961c5f536867d06dfca6eddaf49973b07
renaud lourgouilloux lourgouilloux.renaud@gmail.com <lourgouilloux.renaud@gmail.com>
2024-05-29 17:10:37 +0200

    AddFixeSearch

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from 'mapbox-gl'
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
+    markers: Array,
+    range: Number,
+    location: Object
   }
 
   connect() {
@@ -18,6 +21,7 @@ export default class extends Controller {
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.#addCircleToMap()
 
     this.map.addControl(new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
@@ -43,5 +47,43 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 16, duration: 2000 })
+  }
+
+  #addCircleToMap() {
+    const radiusInMeters = this.rangeValue * 1000 // Convertit la portée en mètres
+    const center = [this.locationValue.lng, this.locationValue.lat] // Centre du cercle
+
+    this.map.on('load', () => {
+      this.map.addSource('circle', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: center
+            }
+          }]
+        }
+      })
+
+      this.map.addLayer({
+        id: 'circle-layer',
+        type: 'circle',
+        source: 'circle',
+        paint: {
+          'circle-radius': {
+            stops: [
+              [0, 0],
+              [20, radiusInMeters / 40075 * 512]
+            ],
+            base: 2
+          },
+          'circle-color': '#007cbf',
+          'circle-opacity': 0.5
+        }
+      })
+    })
   }
 }
